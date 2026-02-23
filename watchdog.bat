6e222f8fff5253fcca64bb08c1770843bb32ae4b
204343414 <107094185+204343414@users.noreply.github.com>
2026-02-24 00:32:24 +0800

    Update watchdog.bat

--- watchdog.bat
+++ watchdog.bat
@@ -7,7 +7,7 @@ set SERVER_EXE=srcds.exe
 set CHECK_INTERVAL=5
 set MAX_FAILS=4
 set FAIL_COUNT=0
-set BOT_URL=http://你的astrBot的服务器IP:9876/gmod_event
+set BOT_URL=http://你的美国服务器IP:9876/gmod_event
 
 :LOOP
 echo [%date% %time%] 检查服务器状态...
@@ -31,20 +31,15 @@ goto LOOP
 :RESTART
 echo [%date% %time%] 服务器崩溃！执行重启...
 
-REM 通知 AstrBot
-curl -s -X POST %BOT_URL% -d "payload={\"event\":\"crash\",\"time\":\"%date% %time%\",\"secret\":\"在这里填同样的密码\",\"data\":{\"message\":\"服务器崩溃已自动重启\"}}" >NUL 2>NUL
+curl -s -X POST %BOT_URL% -d "payload={\"event\":\"crash\",\"time\":\"%date% %time%\",\"data\":{\"message\":\"服务器崩溃已自动重启\"}}" >NUL 2>NUL
 
-REM 强制清理
 taskkill /F /IM %SERVER_EXE% 2>NUL
 timeout /t 5 /nobreak >NUL
 
-REM 重启
 cd /d %SERVER_DIR%
 start "" %SERVER_EXE% -console -game garrysmod +maxplayers 16 +map gm_construct +gamemode sandbox +sv_lan 1
 
 echo [%date% %time%] 服务器已重启
 set FAIL_COUNT=0
 timeout /t 30 /nobreak >NUL
-
 goto LOOP
-
